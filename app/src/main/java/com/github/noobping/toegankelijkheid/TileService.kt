28fc28071bf56e727878cb1978dff67adc5e4c08
nick <noobping@users.noreply.github.com>
2024-08-14 21:07:05 +0200

    Open accessibility settings tile

---
+++ app/src/main/java/com/github/noobping/toegankelijkheid/TileService.kt
@@ -0,0 +1,36 @@
+package com.github.noobping.openaccessibility
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.provider.Settings
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+
+class TileService : TileService() {
+    override fun onClick() {
+        val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+        val pending: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        startActivityAndCollapse(pending)
+    }
+
+    override fun onTileAdded() {
+        qsTile.state = Tile.STATE_INACTIVE
+        qsTile.updateTile()
+    }
+
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+        qsTile.state = Tile.STATE_UNAVAILABLE
+        qsTile.updateTile()
+    }
+
+    override fun onStartListening() {
+        qsTile.state = Tile.STATE_INACTIVE
+        qsTile.updateTile()
+    }
+}
